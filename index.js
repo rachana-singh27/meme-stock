0c9809aa60cafc78da4d9dd41ea518929af50730
Rachana Singh <rachana.singh0172@gmail.com>
2021-02-15 23:32:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.post("/", function(req, res) {
   //res.send("Meme submitted successfully!!");
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("server is running on port 3000.");
+app.listen(process.env.PORT || 8081, function() {
+  console.log("server is running on port 8081.");
 });
